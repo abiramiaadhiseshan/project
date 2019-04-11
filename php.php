90e53007980f13ff2d74b76173c5a5e334d7c40a
abiramiaadhiseshan <49509205+abiramiaadhiseshan@users.noreply.github.com>
2019-04-11 11:33:04 +0530

    Add files via upload

---
+++ php.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="text.php" method="POST">
+	username:<input type="text" name="uname">
+<br>
+	<input type="submit" name="submit" value="OK"><br>
+	<br>
+
+</form>
+</body>
+</html>
